2350c7732fe47dc06d1f3b5c78ff3e880d5778e9
Gstruss <gstrussc@academia.usbbog.edu.co>
2024-05-19 18:46:50 -0500

    conexion bd

---
+++ conexionbd.js
@@ -0,0 +1,20 @@
+let mysql = require('mysql');
+
+let conexionbd = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'asp'
+});
+
+conexionbd.connect(function(err) {
+    if (err) {
+      console.error('Error connecting to database: ' + conexionbd.stack);
+      return;
+    }
+    console.log('Connected to database with id ' + conexionbd.threadId);
+  });
+  
+  module.exports = conexionbd;
+
+  conexionbd.end();
