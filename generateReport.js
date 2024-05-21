74caafc854144ec4e3053d2714a3f599cbf2a17e
Gstruss <gstrussc@academia.usbbog.edu.co>
2024-05-20 19:33:24 -0500

    excel, sirve?

---
+++ generateReport.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql');
+const XLSX = require('xlsx');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'asp'
+});
+
+connection.connect();
+
+const query = 'SELECT * FROM usuarios';
+
+connection.query(query, (error, results, fields) => {
+  if (error) throw error;
+
+  const wb = XLSX.utils.book_new();
+  const ws = XLSX.utils.json_to_sheet(results);
+  
+  XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+  
+  XLSX.writeFile(wb, 'reporte.xlsx');
+  console.log('Reporte generado exitosamente.');
+});
+
+connection.end();
